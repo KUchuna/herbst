1c9cef346b5dae381a4e50c0d0639edce9e8aa74
Ucha Kobakhidze <uchakobakhidze@gmail.com>
2024-08-21 21:40:50 +0400

    minor animations additions

--- components/PartnerCard.jsx
+++ components/PartnerCard.jsx
@@ -3,26 +3,19 @@
 import Image from "next/image"
 import { motion } from "framer-motion"
 
-export default function PartnerCard(props) {
+export default function PartnerCard({ img }) {
+    const childVariants = {
+        hidden: { opacity: 0, translateY: -60 },
+        visible: { opacity: 1, translateY: 0 },
+    }
+
     return (
         <motion.div
-            initial={{
-                opacity: 0,
-                translateY: -60
-            }}
-            whileInView={{
-                opacity: 1,
-                translateY: 0
-            }}
-            transition={{
-                delay: 0.5
-            }} 
-            whileHover={{
-                scale: 1.1
-            }}
-            viewport={{once: false}}
-            className="bg-secondary-light py-8 px-6 rounded-xl cursor-pointer flex items-center justify-center">
-            <Image src={props.img} alt="partner logo"  width={170}/>
+            variants={childVariants}
+            transition={{ duration: 0.5 }}
+            className="bg-secondary-light py-8 px-6 rounded-xl cursor-pointer flex items-center justify-center"
+        >
+            <Image src={img} alt="partner logo" width={170} />
         </motion.div>
     )
 }
--- components/PartnersSection.jsx
+++ components/PartnersSection.jsx
@@ -1,44 +1,49 @@
-import PartnerCard from "./PartnerCard";
+"use client"
+
+import PartnerCard from "./PartnerCard"
 import partner1 from "@/public/assets/partner1.svg"
 import partner2 from "@/public/assets/partner2.svg"
 import partner3 from "@/public/assets/partner3.svg"
 import partner4 from "@/public/assets/partner4.svg"
 import partner5 from "@/public/assets/partner5.svg"
+import { motion } from "framer-motion"
 
 const partners = [
-    {
-        img: partner1
-    },
-    {
-        img: partner2
-    },
-    {
-        img: partner3
-    },
-    {
-        img: partner4
-    },
-    {
-        img: partner5
-    },
+    { img: partner1 },
+    { img: partner2 },
+    { img: partner3 },
+    { img: partner4 },
+    { img: partner5 },
 ]
 
+const containerVariants = {
+    hidden: { opacity: 0 },
+    visible: {
+        opacity: 1,
+        transition: {
+            staggerChildren: 0.3,
+        },
+    },
+}
 
 export default function PartnersSection() {
     return (
         <section className="flex w-full justify-center py-24 items-start font-lora">
             <div className="max-w-large-width px-20 w-full flex flex-col justify-center items-start">
                 <h1 className="text-5xl font-bold">Mitgliedschaften</h1>
-                <div className="pt-12 flex justify-between w-full">
-                    {
-                        partners.map((partner) => (
-                            <PartnerCard
-                                key={partner.img}
-                                img={partner.img}
-                            />
-                        ))
-                    }
-                </div>
+                <motion.div
+                    className="pt-12 flex justify-between w-full"
+                    variants={containerVariants}
+                    initial="hidden"
+                    animate="visible"
+                >
+                    {partners.map((partner, index) => (
+                        <PartnerCard
+                            key={index}
+                            img={partner.img}
+                        />
+                    ))}
+                </motion.div>
             </div>
         </section>
     )
