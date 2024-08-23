58fb5d675f124ba94b5506ad5813de65265f41c8
Ucha Kobakhidze <uchakobakhidze@gmail.com>
2024-08-23 18:15:34 +0400

    fixed minor animations/bugs

--- components/PartnersSection.tsx
+++ components/PartnersSection.tsx
@@ -1,12 +1,12 @@
 "use client"
 
-import PartnerCard from "./PartnerCard"
 import partner1 from "@/public/assets/partner1.svg"
 import partner2 from "@/public/assets/partner2.svg"
 import partner3 from "@/public/assets/partner3.svg"
 import partner4 from "@/public/assets/partner4.svg"
 import partner5 from "@/public/assets/partner5.svg"
-import { motion } from "framer-motion"
+import { animate, motion } from "framer-motion"
+import Image from "next/image"
 
 const partners = [
     { img: partner1 },
@@ -16,17 +16,28 @@ const partners = [
     { img: partner5 },
 ]
 
-const containerVariants = {
-    hidden: { opacity: 0 },
-    visible: {
-        opacity: 1,
+const parentVariants = {
+    initial: {},
+    animate: {
         transition: {
-            staggerChildren: 0.3,
-        },
-    },
+            staggerChildren: 0.3
+        }
+    }
 }
 
-
+const childVariants = {
+    initial: { opacity: 0, y: -60 },
+    animate: {
+        opacity: 1,
+        y: 0,
+        transition: {
+            type: "spring",
+            stiffness: 150,
+            damping: 12,
+            duration: 0.4
+        }
+    },
+}
 
 export default function PartnersSection() {
     return (
@@ -35,16 +46,19 @@ export default function PartnersSection() {
                 <h1 className="text-5xl font-bold">Mitgliedschaften</h1>
                 <motion.div
                     className="pt-12 flex justify-between w-full"
-                    variants={containerVariants}
-                    initial="hidden"
-                    whileInView="visible"
-                    viewport={{ once: false }}
+                    variants={parentVariants}
+                    initial="initial"
+                    whileInView="animate"
+                    viewport={{once: false}}
                 >
                     {partners.map((partner, index) => (
-                        <PartnerCard
+                        <motion.div 
                             key={index}
-                            img={partner.img}
-                        />
+                            className="bg-secondary-light py-8 px-6 rounded-xl cursor-pointer flex items-center justify-center select-none"
+                            variants={childVariants}
+                        >
+                            <Image src={partner.img} alt="partner logo" width={170} />
+                        </motion.div>
                     ))}
                 </motion.div>
             </div>
