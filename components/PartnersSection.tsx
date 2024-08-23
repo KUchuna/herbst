9fbf52b6eed865f528cff52c80c1e6f89ecb6b18
Ucha Kobakhidze <uchakobakhidze@gmail.com>
2024-08-23 18:21:09 +0400

    fixed compilation error

--- components/PartnersSection.tsx
+++ components/PartnersSection.tsx
@@ -5,7 +5,7 @@ import partner2 from "@/public/assets/partner2.svg"
 import partner3 from "@/public/assets/partner3.svg"
 import partner4 from "@/public/assets/partner4.svg"
 import partner5 from "@/public/assets/partner5.svg"
-import { animate, motion } from "framer-motion"
+import { motion } from "framer-motion"
 import Image from "next/image"
 
 const partners = [
@@ -34,7 +34,7 @@ const childVariants = {
             type: "spring",
             stiffness: 150,
             damping: 12,
-            duration: 0.4
+            duration: 0.1
         }
     },
 }
