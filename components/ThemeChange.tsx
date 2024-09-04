d49a8d60497e728063ef125351a57d1132e6384f
Ucha <uchakobakhidze@gmail.com>
2024-09-04 23:57:02 +0400

    changed header theme changer logos

--- components/ThemeChange.tsx
+++ components/ThemeChange.tsx
@@ -1,26 +1,29 @@
 "use client"
 
 import { useTheme } from 'next-themes'
+import dark from "@/public/assets/dark.svg"
+import light from "@/public/assets/light.svg"
+import Image from 'next/image'
 
 export default function ThemeChanger() {
     
     const { setTheme } = useTheme()
     const {resolvedTheme} = useTheme()
 
+    let icon = resolvedTheme == "dark" ? dark : light;
+
     function handleTheme() {
+
         if(resolvedTheme == "dark") {
             setTheme("light")
         } else if(resolvedTheme == "light") {
             setTheme("dark")
         }
-        console.log(resolvedTheme)
     }
 
     return (
-        <div>
-            <button onClick={handleTheme}>
-                theme change
-            </button>
+        <div className='hover:bg-zinc-300 rounded-full p-2' onClick={handleTheme}>
+            <Image src={icon} alt='Change theme' className='select-none' />
         </div>
-    )
+        )
 }
